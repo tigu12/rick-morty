5dc33565593e68f3cb251af2abbc4b3b6f48af02
tigu12 <159773796+tigu12@users.noreply.github.com>
2024-05-30 01:32:43 +0300

    Update Swiper.vue

--- src/components/Swiper.vue
+++ src/components/Swiper.vue
@@ -1,5 +1,18 @@
-<template>
+  <script setup>
+  import { ref } from 'vue'
+  import SwiperCore, { Autoplay } from 'swiper/core'
+  import { Swiper, SwiperSlide } from 'swiper/vue'
+  import 'swiper/swiper-bundle.css'
   
+  SwiperCore.use([Autoplay])
+  const slides = ref([
+    { image: 'https://images7.alphacoders.com/133/1335145.jpg', alt: 'Slide 1' },
+    { image: 'https://c4.wallpaperflare.com/wallpaper/410/59/609/rick-and-morty-tv-rick-sanchez-morty-smith-wallpaper-preview.jpg', alt: 'Slide 2' },
+    { image: 'https://wallpapers.com/images/featured/rick-and-morty-cool-t3ynu5ly9m7jq6e2.jpg', alt: 'Slide 3' },
+  ])
+  </script>
+
+<template>
   <swiper
     class="mySwiper"
     :slides-per-view="1"
@@ -15,22 +28,5 @@
 
   </template>
   
-  <script setup>
-  import { ref } from 'vue'
-  import SwiperCore, { Autoplay } from 'swiper/core'
-  import { Swiper, SwiperSlide } from 'swiper/vue'
-  
-  // Import Swiper's CSS
-  import 'swiper/swiper-bundle.css'
-  
-  // Install Swiper modules
-  SwiperCore.use([Autoplay])
-  
-  // Define your slides data
-  const slides = ref([
-    { image: 'https://images7.alphacoders.com/133/1335145.jpg', alt: 'Slide 1' },
-    { image: 'https://c4.wallpaperflare.com/wallpaper/410/59/609/rick-and-morty-tv-rick-sanchez-morty-smith-wallpaper-preview.jpg', alt: 'Slide 2' },
-    { image: 'https://wallpapers.com/images/featured/rick-and-morty-cool-t3ynu5ly9m7jq6e2.jpg', alt: 'Slide 3' },
-  ])
-  </script>
+
   
