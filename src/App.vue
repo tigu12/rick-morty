9ab811ac8cd40be50dec141aec7b34cd45449292
tigu12 <159773796+tigu12@users.noreply.github.com>
2024-05-30 01:33:36 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,8 @@
 <script setup>
-//import HomePage from './components/HomePage.vue'
-//import EpisodePage from './components/EpisodePage.vue'
-//import CharacterPage from './components/CharacterPage.vue'
-// import LocationPage from './components/LocationPage.vue'
  import { RouterView } from 'vue-router';
 </script>
 
 <template>
- <!-- <HomePage />
-   <EpisodePage />
-    <CharacterPage />
-      <LocationPage />  -->
       <RouterView />
 </template>
 
